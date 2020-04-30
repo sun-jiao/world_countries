3b0db80beb353e089c821dd080b9081a5090ed4a
Sun Jiao <xzzzf.dsx@gmail.com>
2020-04-30 19:44:52 +0800

    Android initial commit

---
+++ app/src/main/java/moe/sunjiao/worldcountries/sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package moe.sunjiao.worldcountries.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
