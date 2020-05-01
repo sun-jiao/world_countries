641d915d72a25f6a343b0f89365020c869d740e0
Sun Jiao <xzzzf.dsx@gmail.com>
2020-05-01 10:16:40 +0800

    Update MainActivity.kt

--- app/src/main/java/moe/sunjiao/worldcountries/sample/MainActivity.kt
+++ app/src/main/java/moe/sunjiao/worldcountries/sample/MainActivity.kt
@@ -2,11 +2,15 @@ package moe.sunjiao.worldcountries.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val locale = Locale("", "CN")
+        name_text.text = locale.displayCountry;
     }
 }
