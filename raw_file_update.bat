7190a9e93df8929e795ebaa61c69f3a27a02b002
Sun Jiao <xzzzf.dsx@gmail.com>
2020-05-01 09:46:06 +0800

    Update raw_file_update.bat

--- raw_file_update.bat
+++ raw_file_update.bat
@@ -1,28 +1,28 @@
-xcopy /s \flags\24x24 \world-countries\src\main\res\mipmap-mdpi
-xcopy /s \flags\32x32 \world-countries\src\main\res\mipmap-hdpi
-xcopy /s \flags\48x48 \world-countries\src\main\res\mipmap-xhdpi
-xcopy /s \flags\64x64 \world-countries\src\main\res\mipmap-xxhdpi
-xcopy /s \flags\128x128 \world-countries\src\main\res\mipmap-xxxhdpi
-xcopy /s \data\ar\*.json \world-countries\src\main\res\raw-ar
-xcopy /s \data\cn\*.json \world-countries\src\main\res\raw-cn
-xcopy /s \data\cs\*.json \world-countries\src\main\res\raw-cs
-xcopy /s \data\da\*.json \world-countries\src\main\res\raw-da
-xcopy /s \data\de\*.json \world-countries\src\main\res\raw-de
-xcopy /s \data\el\*.json \world-countries\src\main\res\raw-el
-xcopy /s \data\en\*.json \world-countries\src\main\res\raw-en
-xcopy /s \data\es\*.json \world-countries\src\main\res\raw-es
-xcopy /s \data\et\*.json \world-countries\src\main\res\raw-et
-xcopy /s \data\fr\*.json \world-countries\src\main\res\raw-fr
-xcopy /s \data\hu\*.json \world-countries\src\main\res\raw-hu
-xcopy /s \data\it\*.json \world-countries\src\main\res\raw-it
-xcopy /s \data\ja\*.json \world-countries\src\main\res\raw-ja
-xcopy /s \data\lt\*.json \world-countries\src\main\res\raw-lt
-xcopy /s \data\nl\*.json \world-countries\src\main\res\raw-nl
-xcopy /s \data\no\*.json \world-countries\src\main\res\raw-no
-xcopy /s \data\pl\*.json \world-countries\src\main\res\raw-pl
-xcopy /s \data\pt\*.json \world-countries\src\main\res\raw-pt
-xcopy /s \data\ro\*.json \world-countries\src\main\res\raw-ro
-xcopy /s \data\ru\*.json \world-countries\src\main\res\raw-ru
-xcopy /s \data\sk\*.json \world-countries\src\main\res\raw-sk
-xcopy /s \data\th\*.json \world-countries\src\main\res\raw-th
-xcopy /s \data\uk\*.json \world-countries\src\main\res\raw-uk
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\flags\24x24\    C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\mipmap-mdpi\     /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\flags\32x32\    C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\mipmap-hdpi\     /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\flags\48x48\    C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\mipmap-xhdpi\    /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\flags\64x64\    C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\mipmap-xxhdpi\   /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\flags\128x128\  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\mipmap-xxxhdpi\  /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\ar\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\ar.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\cn\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\zh.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\cs\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\cs.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\da\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\da.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\de\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\de.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\el\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\el.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\en\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\en.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\es\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\es.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\et\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\et.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\fr\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\fr.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\hu\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\hu.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\it\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\it.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\ja\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\ja.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\lt\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\lt.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\nl\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\nl.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\no\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\no.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\pl\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\pl.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\pt\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\pt.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\ro\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\ro.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\ru\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\ru.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\sk\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\sk.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\th\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\th.csv /Y
+xcopy /s C:\Users\Pozzy\AndroidStudioProjects\world_countries\data\uk\world.csv  C:\Users\Pozzy\AndroidStudioProjects\world_countries\world-countries\src\main\res\raw\uk.csv /Y
